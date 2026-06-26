71c3f0636f2fd96705f54871dd2b8a67c4f5a07a
Perumalla Naresh <naresh.xplores777@gmail.com>
2026-06-26 20:28:15 +0530

    Added solution - LeetHub

---
+++ 704155-count-matching-subsequences-GFG/704155-count-matching-subsequences.java
@@ -0,0 +1,25 @@
+class Solution {
+    static final int MOD = 1000000007;
+
+    public int countWays(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        long[] dp = new long[m + 1];
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = m; j >= 1; j--) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[j] = (dp[j] + dp[j - 1]) % MOD;
+                }
+            }
+        }
+
+        return (int) dp[m];
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
